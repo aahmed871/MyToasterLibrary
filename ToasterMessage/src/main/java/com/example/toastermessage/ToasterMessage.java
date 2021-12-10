f8980460da033afb34f9633344c81a14410356f6
HP <ghulamahmed790@gmail.com>
2021-12-10 11:58:55 +0500

    Completed All Arthmetic Method

--- ToasterMessage/src/main/java/com/example/toastermessage/ToasterMessage.java
+++ ToasterMessage/src/main/java/com/example/toastermessage/ToasterMessage.java
@@ -7,4 +7,16 @@ public class ToasterMessage {
     public static void  s(Context c , String msg){
         Toast.makeText(c, msg, Toast.LENGTH_SHORT).show();
     }
+    public double Addition(double a, double b){
+        return a+b;
+    }
+    public double Subtraction(double a, double b){
+        return a-b;
+    }
+    public double Multiplication(double a, double b){
+        return a*b;
+    }
+    public double Division(double a, double b){
+        return a/b;
+    }
 }
